978d324496533e8ae285fcd64313e7c76c87aa1a
Nishant <nishants440@gmail.com>
2022-10-07 10:45:08 +0530

    Like Button Implementation

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import { app, database } from "./firebaseConfig";
 import {
   GoogleAuthProvider,
   getAuth,
@@ -6,13 +7,10 @@ import {
   onAuthStateChanged,
   signOut,
 } from "firebase/auth";
-import { app, database } from "./firebaseConfig";
-import { collection } from "firebase/firestore";
 import "./App.css";
 
 function App() {
   let [authState, setAuthState] = useState(false);
-  let dbInstance = collection(database, "like-system");
   let auth = getAuth();
   let googleAuth = new GoogleAuthProvider();
   const signIn = () => {
@@ -38,8 +36,15 @@ function App() {
     <div className="App">
       {authState ? (
         <>
-          <button>Like 👍 </button>
-          <button onClick={signout}>Log Out </button>
+          <button onClick={signout} className="logout">
+            Log Out{" "}
+          </button>
+
+          <div className="add-posts">
+            <textarea id="post" name="post" rows="4" cols="50"></textarea>
+            <br />
+            <button>Post</button>
+          </div>
         </>
       ) : (
         <button onClick={signIn}>Sign In with Google</button>
